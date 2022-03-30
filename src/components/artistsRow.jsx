bbc8a02135de41812022e35c12f9d9c0698f481f
Sqbany122 <51026442+Sqbany122@users.noreply.github.com>
2022-03-30 19:52:54 +0200

    images resized

--- src/components/artistsRow.jsx
+++ src/components/artistsRow.jsx
@@ -4,112 +4,112 @@ const ArtistsRow = () => {
             <div className="container padding-5">
                 <div className="row">
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_2.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_2.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_1.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_1.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_3.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_3.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_4.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_4.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_5.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_5.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_6.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_6.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_7.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_7.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_8.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_8.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_9.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_9.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_10.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_10.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_11.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_11.jpg" />
                         <span className="artistName">RudaBroda</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_12.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_12.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_13.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_13.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_14.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_14.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_15.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_15.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
                         <a className="portfolioButton" href="#" target="_blank">Proftfolio</a>
                     </div>
                     <div className="artistBox col-md-4 col-sm-6 text-center">
-                        <img className="artistImg" src="/inc/artysci/artist_16.jpeg" />
+                        <img className="artistImg" src="/inc/artysci/artist_16.jpg" />
                         <span className="artistName">Test test</span>
                         <span className="artistProffesion">Artist</span>
                         <span className="artistStyle">Fine Line, Landscape, Botanical</span>
