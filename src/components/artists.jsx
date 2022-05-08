502fce027fe6d88a2c3aaa519e97b8acbe19400d
Sqbany122 <51026442+Sqbany122@users.noreply.github.com>
2022-05-08 12:45:27 +0200

    Update in artists.jsx, barber-shop.jsx, about.jsx and styles. New image added.

--- src/components/artists.jsx
+++ src/components/artists.jsx
@@ -11,7 +11,7 @@ const Artists = () => {
       <div className="mainView">
         <div className="text col-md-6 col-sm-8 col-12">
           <h1>Artyści</h1>
-          <p>W naszym zespole pracuje niezwykle utalentowana i pozytywna grupa artystów, swobodnie poruszająca się po zróżnicowanych formach i kolorystyce. Każdy posiada swój unikalny styl i wyjątkowe umiejętności, które zaspokoją nawet najbardziej kreatywną i wymagającą wyobraźnię klienta.</p>
+          <p>W naszym zespole pracuje niezwykle utalentowana i pozytywna grupa artystów, swobodnie poruszająca się po zróżnicowanych formach <br />i kolorystyce. Każdy posiada swój unikalny styl i wyjątkowe umiejętności, które zaspokoją nawet najbardziej kreatywną i wymagającą wyobraźnię klienta.</p>
           <p>Nie nakładamy żadnych ograniczeń, co w efekcie daje bardzo szeroką paletę wykonywanych u nas tatuaży.</p>
           <p>Zapraszamy Cię serdecznie do zapoznania się z pracami naszych artystów - jesteśmy przekonani, że znajdziesz coś dla siebie.</p>
         </div>
